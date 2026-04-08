bd60f19c37ed92f9664e4a6fdb3d414dda85eaec
Sarthaki fuley <24fuleys@rbunagpur.in>
2026-04-08 18:11:49 +0530

    fix: make main() function callable with name-main protector

--- server/app.py
+++ server/app.py
@@ -55,4 +55,8 @@ async def state():
 
 def main():
     import uvicorn
+    # Make sure this points to server.app:app
     uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
